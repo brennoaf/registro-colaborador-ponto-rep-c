e3e632645ed574029134e99961b0816965190139
brennoaf <131369370+brennoaf@users.noreply.github.com>
2024-01-09 14:27:48 -0300

    Update script.js
    MANUTENÇÃO DA REMOÇÃO DO COLABORADOR NA ARRAY

--- script.js
+++ script.js
@@ -1,28 +1,21 @@
-nome = document.getElementById("name-input");
+var nome = document.getElementById("name-input");
 pis = document.getElementById("pis-input");
 matricula = document.getElementById("matricula-input");
 submitButton = document.getElementById("submit-button");
 downloadButton = document.getElementById("download-button");
 registroBox = document.getElementById("registros");
 
-let registroCounter = 1; // Inicie o contador
+var lista = []; // ARRAY COM TODO O CONTEÚDO DE REGISTRADOS (REMOVE E ADD)
 
-function resizeRegistros() {
-    var letrasNome = ((nome.value).split(' ')).length
-    var tamanhoNome = (nome.value).length
-    
-    calc = 60 + tamanhoNome / 1.5
-    
-   registroBox.style.height = registroBox.offsetHeight + calc + "px";
-}
+let registroCounter = -1; // INICIADOR DO CONTADOR PARA NOMEAR ID DAS DIVS
 
 
-function pisCheck(pisInput){
+function pisCheck(pisInput){ //PERMITIR SOMENTE NÚMEROS (AJEITAR BUG QUANDO INSERE + ou -)
    return /^[0-9]+$/.test(pisInput);	
 }
 
 function checkForm(){
-   if(!pis.value || !matricula.value || !nome.value){   
+   if(!pis.value || !matricula.value || !nome.value){  // IF PARA MENSAGENS DE ERRO (AJEITAR HIERARQUIA) 
       submitButton.disabled = true;
       document.getElementById("error-message").innerHTML = "Um ou mais campos estão em branco!";
    }else{
@@ -41,12 +34,15 @@ function checkForm(){
    }
 }
 
-var lista = []
 function handleSubmit(event) {
     event.preventDefault();
     
-    nome.value = (nome.value.toUpperCase()).trim()
-    matricula.value = (matricula.value).trim()
+    nome.value = (nome.value.toUpperCase()).trim();
+    matricula.value = (matricula.value).trim();
+
+
+    registNome = nome.value; // REGISTRAR VALOR DO NOME POIS FUTURAMENTE SERÁ NULL
+
     var value = {"Operação":"1+1+I","PIS":pis.value,"Nome":nome.value,"Verificação de Biometria":"1","Qtde Referências":"1","Matricula 1":matricula.value};
     lista.push(value);
     const data = new FormData(event.target);
@@ -61,18 +57,18 @@ function handleSubmit(event) {
     
     document.getElementById("error-message").innerHTML = "Usuário registrado com sucesso!";
     
-    
-    
+
     registroCounter++;
-    registroPagina = document.getElementById("register-page");
-    // Criar uma nova div para cada informação do registro
-    const registroDiv = document.createElement("div");
-    registroDiv.className = "registro-item";
-    registroDiv.id = `regist-func${registroCounter}`;
+    registroPagina = document.getElementById("register-page"); 
+    registroPagina.style.width = '350px';   // DANDO TAMANHO À PÁGINA COM OS REGISTROS
+
+    const registroDiv = document.createElement("div"); // CRIANDO DIV
+    registroDiv.className = "registro-item"; // DANDO CLASSE
+    registroDiv.id = `regist-func${registroCounter}`; // DANDO ID
+    registroDiv.setAttribute("name", registNome); // SETANDO NOME DA CLASSE
     
-    document.querySelector(".register-list").appendChild(registroDiv);
+    document.querySelector(".register-list").appendChild(registroDiv); // ADICIONANDO DIV DO COLABORADOR À DIV PAI
    
-    registroPagina.style.width = '350px';    
     
     // Adicionar informações do registro à div
     registroDiv.innerHTML = `
@@ -80,42 +76,53 @@ function handleSubmit(event) {
         <p><strong>PIS:</strong> ${value.PIS}</p>
         <p><strong>Matrícula:</strong> ${value["Matricula 1"]}</p>
         <button class="remove-button" onclick="removeRegistro('${registroDiv.id}')">X</button>
-    `;
-    // Adicionar a nova div à lista de registros
+    `; //INSERINDO CONTEÚDO NA DIV DO COLABORADOR
+
     registroDiv.style.width = '320px'; 
+    console.log(lista)
 }
  
 
 
 function removeRegistro(registroId) {
-    const registroDiv = document.getElementById(registroId);
-    registroDiv.remove();
-    
-    const registroContainer = document.querySelector('.register-list');
+   const registroDiv = document.getElementById(registroId);
+   getDivName = registroDiv.getAttribute("name"); // PEGA O NOME DA DIV DO COLABORADOR PARA UTILIZAR NO FOR
+   
+    for(linhas in lista){
+       for(linha in linhas){
+         if (getDivName == (lista[linhas[linha]].Nome)){ //CHECAR SE O NOME DA DIV DO COLABORADOR (QUE É IGUAL AO NOME DO COLABORADOR NA LISTA) É IGUAL AO NOME NO OBJ ATUAL DA CHECAGEM
+            console.log(getDivName + ' foi removido com sucesso!')
+            lista.splice(linhas, 1)
+         }else {
+            console.log('Erro! Entre em contato com o desenvolvedor\nTambém pode reiniciar a página (Perde todo o conteúdo)')
+         }
+      }
+    }
 
-if (registroContainer && registroContainer.children.length === 0) {
-    registroPagina = document.getElementById("register-page");
-    registroPagina.style.width = '0';
-} 
+    registroDiv.remove(); //REMOVE DIV DO COLABORADOR
+
+    const registroContainer = document.querySelector('.register-list');
+   if (registroContainer && registroContainer.children.length === 0) { //CHECAR SE A DIV PAI ESTÁ VAZIA, PARA ESCONDER DA PÁGINA TOTALMENTE
+      registroPagina.style.width = '0';
+   }
 }
 
  
-function downloadResultados(){
+function downloadResultados(){ // EXECUTAR DOWNLOAD
     console.log(lista);
 
-    var jsonData = JSON.stringify(this.lista);
-    console.log(jsonData);
+    var jsonData = JSON.stringify(lista); // TRANSFORMA EM JSON STRING
+    console.log('CONTEÚDO BAIXADO:\n\n' + jsonData);
 
     var blob = new Blob([jsonData], { type: 'application/json' });
-    var a = document.createElement('a');
+    var a = document.createElement('a'); // CRIANDO ÂNCORA
     a.href = window.URL.createObjectURL(blob);
-    a.download = 'rep_colaborador.json';
+    a.download = 'rep_colaborador.json'; // NOME DO ARQUIVO
     
-    document.body.appendChild(a);
+    document.body.appendChild(a); //ADICIONAR ÂNCORA
     a.click();
-    document.body.removeChild(a);
+    document.body.removeChild(a); // REMOÇÃO DE ÂNCORA
 }
-
 
 
 
@@ -125,3 +132,13 @@ nome.addEventListener('input', checkForm);
 
 const form = document.querySelector("form");
 form.addEventListener("submit", handleSubmit);
+
+
+// function resizeRegistros() {
+//     var letrasNome = ((nome.value).split(' ')).length 
+//     var tamanhoNome = (nome.value).length
+    
+//    calc = 60 + tamanhoNome / 1.5
+    
+//   registroBox.style.height = registroBox.offsetHeight + calc + "px";
+//} INUTILIZADO MAS GUARDADO PARA INSERÇÃO DA LISTAGEM FÍSICA (DIV DE ID #registros)
